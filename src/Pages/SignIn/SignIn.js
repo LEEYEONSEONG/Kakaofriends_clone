a5a478434221986c1a1bd93fea557a9d77130252
leeyeonseong <dustjd1535@gmail.com>
2020-09-16 17:18:18 +0900

    [12기 이연성] Add: Singin Page 레이아웃 리뷰 반영

--- src/Pages/SignIn/SignIn.js
+++ src/Pages/SignIn/SignIn.js
@@ -1,49 +1,91 @@
 import React from "react";
 import "./SignIn.scss";
+// import USER from "./data";
 
-const INFO = [
-  "이용약관",
-  "개인정보 처리방침",
-  "운영정책",
-  "고객센터",
-  "공지사항",
-  "한국어",
-];
+const errorTextOne = "이메일 형식이 올바르지 않습니다.";
+const errorNum = "이메일을 입력해주세요.";
+// const errorNumFore = "이메일 형식이 맞아";
+// "카카오계정 혹은 비밀번호가 일치하지 않습니다. 입력한 내용을 다시 확인해 주세요.";
 
 class SignIn extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      checkbox: "checkBox",
-      toolTip: "toolTip none",
-      errorBox: "errorBox none",
+      isChecked: false,
+      isToolTip: false,
+      isErrorBox: "errorBox none",
+      idValue: "",
+      pwdValue: "",
+      errorText: "",
+      isClick: false,
     };
   }
 
-  handleCheckBox = () => {
-    const { checkbox } = this.state;
-    this.setState({
-      checkbox: checkbox === "checkBox" ? "checkBox active" : "checkBox",
-    });
+  handleIdPwValue = (e) => {
+    const { value, name } = e.target;
+    this.setState({ [name]: value }, () => this.valid());
   };
 
-  handleToolTip = () => {
-    const { toolTip } = this.state;
-    this.setState({
-      toolTip: toolTip === "toolTip" ? "toolTip none" : "toolTip",
-    });
+  valid = () => {
+    const { idValue } = this.state;
+
+    if (idValue.length < 1) {
+      this.setState({
+        errorText: errorNum,
+        // isErrorBox: "errorBox",
+      });
+      return;
+    } else {
+      if (!idValue.includes("@")) {
+        this.setState({
+          errorText: errorTextOne,
+          // isErrorBox: "errorBox",
+        });
+        return;
+      } else {
+        if (idValue.includes(".com") || idValue.includes(".net")) {
+          this.setState({
+            errorText: "",
+            isErrorBox: "errorBox none",
+          });
+          return;
+        }
+      }
+    }
   };
 
-  handleErrorBox = () => {
-    const { errorBox } = this.state;
-    this.setState({
-      errorBox: errorBox === "errorBox" ? "errorBox none" : "errorBox",
-    });
+  handleClick = () => {
+    if (this.state.errorText.length) {
+      this.setState({
+        isErrorBox: "errorBox",
+      });
+    } else {
+      this.setState({
+        isErrorBox: "errorBox none",
+      });
+    }
   };
+  // this.setState({ isClick: true });
+  // fetch("http://localhost:3000/data/signinmock.json", {
+  //   method: "POST",
+  //   body: JSON.stringify({
+  //     userEmail: this.state.idValue,
+  //     password: this.state.pwdValue,
+  //   }),
+  // })
+  //   .then((response) => {
+  //     console.log(response);
+  //     return response.json();
+  //   })
+  //   .then((result) => {
+  //     console.log(result);
+  //     console.log(result.USER[0].userEmail === this.state.idValue);
+  //   });
 
   render() {
-    const { checkbox, toolTip, errorBox } = this.state;
+    const { isChecked, isToolTip, isErrorBox, errorText } = this.state;
+    const { idValue, pwdValue } = this.state;
     return (
       <div className="SignIn">
         <main className="wrapper">
@@ -54,50 +96,64 @@ class SignIn extends React.Component {
               </strong>
               <p className="bannerFontBot">
                 Kakao의 모든 서비스 뿐 아니라 Melon, Daum등 다른 다양한
-                서비스까지 <br></br>이제 카카오계정으로 이용해 보세요!
+                서비스까지 <br />
+                이제 카카오계정으로 이용해 보세요!
               </p>
             </div>
             <div className="bannerImg">
-              <img src={"Images/banner.png"} alt="banner" />
+              <img alt="banner" src="Images/banner.png" />
             </div>
           </div>
           <div className="wrapperRight">
             <div className="infoLogo">
               <img
                 className="infoServiceLogo"
-                src={"/Images/bannerlogo.png"}
                 alt="로고"
+                src="/Images/bannerlogo.png"
               ></img>
             </div>
             <div className="loginForm">
               <div className="loginTop">
                 <input
                   className="idPwForm"
+                  onChange={this.handleIdPwValue}
                   placeholder="카카오계정(이메일 또는 전화번호)"
                   type="text"
+                  value={idValue}
+                  name="idValue"
                 ></input>
                 <input
                   className="idPwForm"
+                  onChange={this.handleIdPwValue}
                   placeholder="비밀번호"
                   type="password"
+                  value={pwdValue}
+                  name="pwdValue"
                 ></input>
                 <div className="setLogin">
                   <span
-                    className={checkbox}
-                    onClick={this.handleCheckBox}
+                    className={isChecked ? "checkBox active" : "checkBox"}
+                    onClick={() => this.setState({ isChecked: !isChecked })}
                   ></span>
-                  <span onClick={this.handleToolTip}>로그인 상태 유지</span>
-                  <div className={toolTip}>
+                  <span
+                    onClick={() => this.setState({ isToolTip: !isToolTip })}
+                  >
+                    로그인 상태 유지
+                  </span>
+                  <div className={isToolTip ? "toolTip" : "toolTip none"}>
                     <p>개인정보 보호를 위해 개인 PC에서만 사용해 주세요.</p>
                     <a href="www">도움말 보기</a>
                   </div>
                 </div>
               </div>
               <div className="loginBtm">
-                <div className={errorBox}>
-                  <p>이메일 또는 전화번호를 입력해주세요.</p>
+                <div className={isErrorBox}>
+                  {/* <p>{idValue ? "이메일" : "이메일 또는 전화번호를 입력해주세요."}</p> */}
+                  <p>{errorText}</p>
+                  {/* <p>이메일 또는 전화번호를 입력해주세요.</p> */}
+                  {/* <p>{wrongOne}</p> */}
                 </div>
-                <button className="btn" onClick={this.handleErrorBox}>
+                <button className="btn" onClick={this.handleClick}>
                   로그인
                 </button>
                 <div className="line">
@@ -106,7 +162,7 @@ class SignIn extends React.Component {
                   <div className="inLine" />
                 </div>
                 <button className="qrCode">
-                  <img src={"/Images/qrimage.png"} alt="qr"></img>QR코드 로그인
+                  <img src="/Images/qrimage.png" alt="qr"></img>QR코드 로그인
                 </button>
               </div>
             </div>
@@ -144,3 +200,12 @@ class SignIn extends React.Component {
 }
 
 export default SignIn;
+
+const INFO = [
+  "이용약관",
+  "개인정보 처리방침",
+  "운영정책",
+  "고객센터",
+  "공지사항",
+  "한국어",
+];
