33729c0baf7a5e114629c93f378d4634d658214d
jinheekim <jinheeee72@gmail.com>
2020-09-19 15:54:46 +0900

    ADD : 빈장바구니(Cart) 레이아웃 생성

--- src/Pages/Cart/Cart.js
+++ src/Pages/Cart/Cart.js
@@ -5,12 +5,17 @@ class Cart extends React.Component {
   render() {
     return <div className="Cart">
       <div className="headerCartBorder">
-      둘레
       <div className="headerCart">
        장바구니
       </div>
       <div className="emptyCart">
-       빈 장바구니
+       <div className="emptyCartMessage">
+         <p>아직 관심 상품이 없네요!</p>
+         <p>귀여운 프렌즈 상품을 추천드릴게요</p> 
+       </div>
+      <a href="https://store.kakaofriends.com/kr/index?tab=hot" className="emptyCartLink">
+        인기 상품 보기
+      </a>
       </div>
     </div>
     </div>;
