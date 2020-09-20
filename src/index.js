4bb2aefadc77a93f1db6926b8efcc289c329d4b2
jaedie <jamesleehw@gmail.com>
2020-09-20 14:11:15 +0900

    resolved conflicts

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Routes from './Routes';
-import './Styles/reset.scss';
-
-ReactDOM.render(<Routes />, document.getElementById('root'));
-=======
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
@@ -13,4 +5,3 @@ import "./Styles/reset.scss";
 import "./Styles/common.scss";
 
 ReactDOM.render(<Routes />, document.getElementById("root"));
->>>>>>> master
