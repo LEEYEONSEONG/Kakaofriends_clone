cbb1187174e8074fc31eecaea68e4a5e6c09e9da
MiaJLee <angella555@naver.com>
2020-09-23 22:00:33 +0900

    Add: 검색 결과 없을 경우의 수 고려하여 코드 수정

--- src/Pages/SearchResult/SearchResult.js
+++ src/Pages/SearchResult/SearchResult.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Nav from "./../../Components/Nav/Nav";
+import Footer from "./../../Components/Footer/Footer";
 import Sorting from "../../Components/Sorting/Sorting";
 import List from "../../Components/List/List";
 import URL from "../../url";
@@ -26,10 +27,17 @@ class SearchResult extends React.Component {
       .then((res) => res.json())
       .then((res) => {
         console.log(res);
-        this.setState({
-          productList: res.data_list,
-          totalCount: res.data_list[0].total_count,
-        });
+        if (res.data_list.length) {
+          this.setState({
+            productList: res.data_list,
+            totalCount: res.data_list[0].total_count,
+          });
+        } else {
+          this.setState({
+            productList: [],
+            totalCount: "",
+          });
+        }
       });
   };
 
@@ -47,26 +55,45 @@ class SearchResult extends React.Component {
     console.log(this.state.productList);
     const { productList, totalCount } = this.state;
     return (
-      <div className="SearchResult">
-        <Nav />
-        <div className="resultKeywords">
-          <h1
-            dangerouslySetInnerHTML={{
-              __html: `<span>‘${this.props.history.location.state}’</span> 검색결과`,
-            }}
-          />
-        </div>
-        <div className="topInfo">
-          <div className="totalItems">
-            <p className="totalNum">
-              총 <span className="pointSpan">{totalCount}</span> 개의 상품이
-              조회되었습니다.
-            </p>
+      <>
+        <div className="SearchResult">
+          <Nav />
+          <div className="resultKeywords">
+            <h1
+              dangerouslySetInnerHTML={{
+                __html: `<span>‘${this.props.history.location.state}’</span> 검색결과`,
+              }}
+            />
           </div>
-          <Sorting />
+          {this.state.productList.length ? (
+            <>
+              <div className="topInfo">
+                <div className="totalItems">
+                  <p className="totalNum">
+                    총 <span className="pointSpan">{totalCount}</span> 개의
+                    상품이 조회되었습니다.
+                  </p>
+                </div>
+                <Sorting />
+              </div>
+              <List productList={productList} />
+            </>
+          ) : (
+            <div className="notFound">
+              <img
+                className="ryanConfused"
+                src="https://t1.kakaocdn.net/friends/new_store/2.0/common/img_empty_ryan.png"
+              />
+              <h3>검색결과가 없습니다</h3>
+              <p>
+                다른 검색어를 입력하시거나,
+                <br /> 철자 및 띄어쓰기를 확인해주세요.
+              </p>
+            </div>
+          )}
         </div>
-        <List productList={productList} />
-      </div>
+        <Footer />
+      </>
     );
   }
 }
