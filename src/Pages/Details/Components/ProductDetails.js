6201639462413014717ec9db5ab2fad3a4b991bc
jaedie <jamesleehw@gmail.com>
2020-09-23 11:57:29 +0900

    ProductDetails 페이지 수정

--- src/Pages/Details/Components/ProductDetails.js
+++ src/Pages/Details/Components/ProductDetails.js
@@ -1,8 +1,38 @@
 import React, { Component } from "react";
+import "../Details.scss";
 
 export class ProductDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      productDetails: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://10.58.2.50:8001/products/product/250")
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({
+          productDetails: res.data_list,
+        });
+      });
+  }
+
   render() {
-    return <div></div>;
+    console.log(this.state.productDetails);
+    return (
+      <div>
+        {this.state.productDetails.map((el) => (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: el.detail,
+            }}
+          />
+        ))}
+      </div>
+    );
   }
 }
 
